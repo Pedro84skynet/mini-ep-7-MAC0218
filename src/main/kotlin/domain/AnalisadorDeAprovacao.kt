1910e00685ad5a8aa4f1b8cd5a9626b4541b6f75
Pedro84skynet <pedro2.fernandes@usp.br>
2022-05-11 11:35:56 -0300

    refactor(MiniEp7): Add implementation of AnalisadorDeAprovacao

--- src/main/kotlin/domain/AnalisadorDeAprovacao.kt
+++ src/main/kotlin/domain/AnalisadorDeAprovacao.kt
@@ -1,15 +1,22 @@
 package domain
 
+import domain.criterios.Basico
+import domain.criterios.CriterioDeAprovacao
+import domain.criterios.Intermediario
+import domain.criterios.Rigoroso
+import domain.Boletim
+import domain.BoletimFechado
+
 class AnalisadorDeAprovacao {
+    private lateinit var criterio: CriterioDeAprovacao
 
-    // ---------------------------------
-    //
-    //      Seu código deve vir aqui
-    //
-    //      Defina atributos e métodos conforme especificado
-    //      no arquivo de teste encontrado em
-    //      'src/test/kotlin/domain/AnalisadorDeAprovacaoTest'
-    //
-    // ---------------------------------
+    fun defineCriterio(criterio : CriterioDeAprovacao) {
+        this.criterio = criterio
+    }
 
+    fun fechaBoletim(boletim : Boletim): BoletimFechado{
+        var boletimF: BoletimFechado = BoletimFechado(boletim.mediaEPs, boletim.mediaMiniEPs,
+            criterio.mediaFinal(boletim), criterio.estaAprovado(boletim))
+        return boletimF
+    }
 }
